8e418bcdeb9e5e66a8a46b80375d4b7466e6bb60
Will Harris <willharris@Wills-MacBook-Air.local>
2026-03-15 20:04:52 -0700

    added dummy checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Grid'
+import Divider from '@mui/material/Divider'
+import Stack from '@mui/material/Stack'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
 import { styled } from '@mui/material/styles'
 
 const EventImage = styled('img')({
@@ -23,8 +27,8 @@ interface Event {
 function App() {
   const [output, setOutput] = useState<Event[]>([])
   const [url] = useState('https://www.meetup.com/find/?location=us--or--Portland&source=EVENTS&customStartDate=2026-03-21T03%3A00%3A00-04%3A00&customEndDate=2026-03-22T02%3A59%3A59-04%3A00&eventType=inPerson&distance=twentyFiveMiles')
-
-  async function handleClick() {
+  
+  async function getEvents() {
     const html = await window.ipcRenderer.invoke('fetch-example', url)
     const doc = new DOMParser().parseFromString(html, 'text/html')
     const events = Array.from(doc.querySelectorAll('a[data-event-label="Event Card"]')).map(a => ({
@@ -40,7 +44,12 @@ function App() {
 
   return (
     <>
-      <Button variant="contained" onClick={handleClick}>Hello World!!</Button>
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained" onClick={getEvents}>Get Events</Button>
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Meetup" />
+        <FormControlLabel control={<Checkbox defaultChecked/>} label="Eventbrite" />
+      </Stack>
+      <Divider/>
       <Grid container spacing={2}>
         {output.map((event, i) => (
           <Grid key={i} size={{ xs: 12, sm: 6, md: 4 }}>
@@ -56,6 +65,10 @@ function App() {
             </Card>
           </Grid>
         ))}
+      </Grid>
+      <Divider/>
+      <Grid container spacing={2}>
+
       </Grid>
     </>
   )
