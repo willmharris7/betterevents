2d1e3ba332e6e130ab0c8998f4fa10016a67b03f
Will Harris <willharris@Wills-MacBook-Air.local>
2026-03-28 13:30:36 -0700

    removed require to allow build

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,8 @@
 import { app, BrowserWindow } from 'electron'
 import { Utils } from './utils'
-import { createRequire } from 'node:module'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 
-const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // The built directory structure
